0859412bced46be409a622abf0b13b85b8b2d909
Nimo Hsieh <nimo1491@gmail.com>
2019-07-23 17:09:26 +0800

    Update scripts

--- offline_install.sh
+++ offline_install.sh
@@ -9,7 +9,10 @@ vim_base="$(pwd)"
 
 # vimrc and ycm conf
 ln -sf $vim_base/vimrc $HOME/$VIMRC
-ln -sf $vim_base/ycm_extra_conf.py $HOME/$YCMCONF
+ln -sf $vim_base/plugged/YouCompleteMe/third_party/ycmd/examples/.ycm_extra_conf.py $HOME/$YCMCONF
+
+# Re-compiler ycm
+$vim_base/plugged/YouCompleteMe/install.py --clang-completer
 
 # finish
 echo "done"
